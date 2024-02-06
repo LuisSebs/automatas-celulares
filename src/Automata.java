97e339b8adc7397d9d0f4b82ad09836d91057760
LuisSebs <sebastian_luis@ciencias.unam.mx>
2024-02-06 12:13:12 -0600

    feat: codigo documentado

--- src/Automata.java
+++ src/Automata.java
@@ -2,13 +2,19 @@ package src;
 
 import processing.core.PApplet;
 
+/**
+ * Automata celular de una dimension
+ */
 public class Automata extends PApplet {
 
+    /** Cantidad de celdas a lo ancho */
     int ancho = 61;
+    /** Cantidad de celdas a lo largo */
     int alto = 60;
+    /** Tamaño en pixeles de una celda */
     int celda = 10;
+    /** Representacion logica del automata */
     ModeloAutomata modelo;
-    Regla regla;
 
     @Override
     public void setup(){
@@ -37,17 +43,32 @@ public class Automata extends PApplet {
                 }
             }
         }
+        // Generamos la siguiente generacion
         modelo.siguienteGeneracion();
     }
 
+    /**
+     * Representacion logica de un automata
+     */
     class ModeloAutomata extends Thread {
 
+        /** Cantidad de celdas a lo ancho */
         public int ancho;
+        /** Cantidad de celdas a lo largo */
         public int alto;
+        /** Regla a aplicar */
         Regla regla;
+        /** Generacion actual */
         public int generacion = 0;
+        /** Tablero del automata */
         public int[][] mundo;
         
+        /**
+         * Constructor del automata
+         * @param ancho cantidad de celdas a lo ancho
+         * @param alto cantidad de celdas a lo largo
+         * @param regla regla a aplicar
+         */
         public ModeloAutomata(int ancho, int alto, Regla regla){
             this.ancho = ancho;
             this.alto = alto;
@@ -56,6 +77,9 @@ public class Automata extends PApplet {
             this.mundo[(int) (ancho/2)][0] = 1; // Primera Generacion
         }
 
+        /**
+         * Genera la siguiente generacion
+         */
         public void siguienteGeneracion(){
 
             if (generacion == alto - 1){
@@ -63,11 +87,10 @@ public class Automata extends PApplet {
             }
 
             for (int i = 1; i < ancho - 1 ; i++){
-                int b1 = mundo[i-1][generacion];
-                int b2 = mundo[i][generacion];
-                int b3 = mundo[i+1][generacion];
-                int resultado = regla.regla(b1, b2, b3);
-                mundo[i][generacion + 1] = resultado;
+                int b1 = mundo[i-1][generacion]; // bit 1
+                int b2 = mundo[i][generacion]; // bit 2
+                int b3 = mundo[i+1][generacion]; // bit 3
+                mundo[i][generacion + 1] = regla.regla(b1, b2, b3);
             }
             generacion++;
         }
--- src/Regla.java
+++ src/Regla.java
@@ -3,8 +3,19 @@ import java.util.HashMap;
 
 public class Regla {
 
+    /**
+     * Reglas
+     */
     HashMap<String, Integer> reglas = new HashMap<>();
     
+    /**
+     * Aplica la regla
+     * @param b1 bit1
+     * @param b2 bit2
+     * @param b3 bit3
+     * @return el resultado de la combinacion
+     * de b1, b2 y b3
+     */
     public int regla(int b1, int b2, int b3){
         return reglas.get(""+b1+b2+b3);
     }
